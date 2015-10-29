3243027387e00bf775a4e410dbde7690bc759d5c
ecayer <elizabeth.ayer@red-gate.com>
2015-10-29 16:12:12 +0000

    Add some comments

--- SOCTFSLinking.ps1
+++ SOCTFSLinking.ps1
@@ -1,10 +1,26 @@
-﻿$socPath = $env:LOCALAPPDATA + "\Red Gate\SQL Source Control 4\"
-[xml]$linkedDatabases = Get-Content C:\Temp\LinkedDatabases.xml # Change this to the location of your master LinkedDatabases.xml file
+# Powershell script to hook up databases to SQL Source Control.
+#
+# Currently only for Shared-mode databases and TFS.
+# Needs to be run on the SSMS client machine, with SQL Source Control already installed, logged in with the correct user.
+# Requires Microsoft Visual Studio Team Foundation Server Power Tools to be installed.
+# It will overwrite any existing config, so any current database links will be lost.
+
+# Currently hard-coded to version 4.
+$socPath = $env:LOCALAPPDATA + "\Red Gate\SQL Source Control 4\"
+
+# Change this to the location of your master LinkedDatabases.xml file
+[xml]$linkedDatabases = Get-Content C:\Temp\LinkedDatabases.xml 
+
+# Get the list of all databases linked to TFS in the master xml file
 $databaseNodes = $linkedDatabases.LinkedDatabaseStore.LinkedDatabaseList.value | where {$_.ISrcCLocation.type -eq 'TfsLocation'}
+
+# Recreate the linking on the local machine
 foreach ($linkedDatabase in $databaseNodes) 
 {
         $tfs = get-tfs($linkedDatabase.ISrcCLocation.ServerUrl)
         $tfs.Authenticate()
+
+	# Set a SQL Source Control workspace
         $randomFileName = [System.IO.Path]::GetRandomFileName()
         $randomDirectoryName = Join-Path (Join-Path $socPath WorkingBases) $randomFileName
         $workspace = $tfs.VCS.CreateWorkspace("SQL Source Control (" + $randomFileName + ")", $tfs.AuthenticatedUserName, "Used by SQL Source Control - do not modify")
@@ -14,7 +30,7 @@ foreach ($linkedDatabase in $databaseNodes)
         $node = $linkedDatabase.IWorkspaceId
         $node.RootPath = $randomDirectoryName.ToString()
     
-
+	# Set a SQL Source Control location for transient files
         $randomFileName = [System.IO.Path]::GetRandomFileName()
         $randomDirectoryName = Join-Path (Join-Path $socPath Transients) $randomFileName
         $workspace = $tfs.VCS.CreateWorkspace("SQL Source Control (" + $randomFileName + ")", $tfs.AuthenticatedUserName, "Used by SQL Source Control - do not modify")
@@ -33,7 +49,7 @@ function get-tfs (
 )
 {
 
-# load the required dll
+    # load the required dll
     [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.Client")
 
     $propertiesToAdd = (
@@ -46,7 +62,7 @@ function get-tfs (
 
    
 
-# fetch the TFS instance, but add some useful properties to make life easier
+    # fetch the TFS instance, but add some useful properties to make life easier
     # Make sure to "promote" it to a psobject now to make later modification easier
     [psobject] $tfs = [Microsoft.TeamFoundation.Client.TeamFoundationServerFactory]::GetServer($serverName)
     foreach ($entry in $propertiesToAdd) {
